68e9f6e2eec29390f69be2c61691494930a09c5c
Vicxwild <vicxwild@gmail.com>
2022-12-22 19:03:02 +0300

    add: creating and deleting questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :show]
-  before_action :find_question, only: [:show]
+  before_action :find_test, only: [:index, :show, :new, :create, :destroy]
+  before_action :find_question, only: [:show, :destroy]
 
   def index
     @questions = @test.questions
@@ -8,6 +8,25 @@ class QuestionsController < ApplicationController
 
   def show
     render plain: @question.body
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to test_questions_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    # render plain: 'The question #{@question.id} has deleted!'
+    redirect_to test_questions_path(@test)
   end
 
   private
@@ -18,5 +37,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = @test.questions.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body).to_h
   end
 end
