8c854817f5d7e2d55a38ebd5ec686558b5876271
Vicxwild <vicxwild@gmail.com>
2023-01-24 18:45:19 +0300

    feat: generate test_passages controller, create templates of actionsand routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   resources :tests, only: %i[index show] do
     resources :questions do
-      resources :answers
+      resources :answers, except: :index
+    end
+  end
+
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
     end
   end
 end
