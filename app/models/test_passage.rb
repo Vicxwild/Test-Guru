67155787c47ae38249e2f24c897f0cb2ef5556a0
Vicxwild <vicxwild@gmail.com>
2023-01-26 12:36:39 +0300

    feat: add start action to test controller and test_passage method to User model

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,4 +1,5 @@
 class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
+  belongs_to :correct_questions, class_name: 'Question', optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2023_01_24_142517) do
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "correct_questions"
+    t.integer "correct_questions", default: 0
     t.integer "current_question_id"
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
