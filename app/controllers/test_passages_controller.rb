0fc423507407fc4a44f0378f18f52d7207f1911d
Vicxwild <vicxwild@gmail.com>
2023-01-26 17:37:07 +0300

    feat: add callback for searching test_passage instance

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,5 +1,7 @@
 class TestPassagesController < ApplicationController
 
+  before_action :set_test_passage, only: %i[show result update]
+
   def show
   end
 
@@ -7,5 +9,11 @@ class TestPassagesController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
   end
 end
