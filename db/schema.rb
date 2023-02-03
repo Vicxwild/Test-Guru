f306d8804c5754ffbcd403b316abbe5ffe730f77
Vicxwild <vicxwild@gmail.com>
2023-02-03 17:32:08 +0300

    add user email uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_02_093259) do
+ActiveRecord::Schema.define(version: 2023_02_03_141708) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2023_02_02_093259) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
