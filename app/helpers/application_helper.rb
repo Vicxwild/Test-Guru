60f067a28c48e39ed4ed3efd81e2448b3fb26db3
Vicxwild <vicxwild@gmail.com>
2023-02-08 17:10:11 +0300

    fix: return flash-messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'TestGuru project', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_messages
+    flash.map do |key, message|
+      content_tag :p, message, class: "flash #{key}"
+    end.join.html_safe
+  end
 end
