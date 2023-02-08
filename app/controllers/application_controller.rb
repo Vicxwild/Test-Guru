e49c9149ef1d5c4cd73c56bf2e56463392b199d3
Vicxwild <vicxwild@gmail.com>
2023-02-08 17:04:29 +0300

    feat: deleting session files and configuring authentication via devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,4 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  helper_method :current_user,
-                :logged_in?
-
-  private
-
-  def authenticate_user!
-    unless current_user
-      cookies[:requested_page] = request.fullpath
-      redirect_to login_path
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
 end
