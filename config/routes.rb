969ce785ddd7a36a0f7e8dd19ce1e71a3267148c
Vicxwild <vicxwild@gmail.com>
2023-02-14 15:14:24 +0300

    refactor: remove the ability to manage resources by the user

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests, only: :index do
-    resources :questions do
-      resources :answers, except: :index
-    end
-
     member do
       post :start
     end
