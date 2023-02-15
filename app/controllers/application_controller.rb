8bb94af4fa84897ec0a37bc7bc5d6740129f5704
Vicxwild <vicxwild@gmail.com>
2023-02-15 08:45:23 +0300

    fix: change the resource provided for referral to a specific page when logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    current_user.admin? ?  admin_tests_path : root_path
+    resource.admin? ?  admin_tests_path : root_path
   end
 
   def configure_permitted_parameters
