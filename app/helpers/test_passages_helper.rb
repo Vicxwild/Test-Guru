3ef5f34fff9675430281e98ac687b8e296435265
Vicxwild <vicxwild@gmail.com>
2023-02-20 16:06:44 +0300

    feat: add en/ru localization for test passages

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,9 @@
 module TestPassagesHelper
   def test_result(test_passage)
     if test_passage.success?
-      "<p> Your result: <span class='success'>#{@test_passage.percentage_success}%</span> The test was passed successfully </p>".html_safe
+      "<p> #{t('.result')}: <span class='success'>#{@test_passage.percentage_success}%</span> - #{t('.passed')} </p>".html_safe
     else
-      "<p> Your result: <span class='fail'>#{@test_passage.percentage_success}%</span> The test failed </p>".html_safe
+      "<p> #{t('.result')}: <span class='fail'>#{@test_passage.percentage_success}%</span> - #{t('.failed')} </p>".html_safe
     end
   end
 end
