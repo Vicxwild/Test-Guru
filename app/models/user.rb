0577bb2be01848505c084e87d9bf5aa666357e1e
Vicxwild <vicxwild@gmail.com>
2023-03-01 11:27:28 +0300

    feat: add associations and validations for gist

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "creator_id"
+  has_many :gists
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: true
