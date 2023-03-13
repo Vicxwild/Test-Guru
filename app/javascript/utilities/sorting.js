78affd37a8d6fd6a10d91e3375de93936d50d2f7
Vicxwild <vicxwild@gmail.com>
2023-03-13 13:38:29 +0300

    feat: finalize sorting by title users table

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,7 +1,7 @@
 document.addEventListener('turbolinks:load', function() { // это что за анонимная функция как и зачем она
   var control = document.querySelector('.sort-by-title')
 
-  control.addEventListener('click', sortRowsByTitle)
+  if (control) { control.addEventListener('click', sortRowsByTitle) }
 })
 
 function sortRowsByTitle() {
@@ -17,25 +17,45 @@ function sortRowsByTitle() {
     sortedRows.push(rows[i])
   }
 
-  sortedRows.sort(compareRows)
+  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+    sortedRows.sort(compareRowsAsc)
+    this.querySelector('.octicon-arrow-up').classList.remove('hide')
+    this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide')
+  }
 
   var sortedTable = document.createElement('table')
+  sortedTable.classList.add('table', 'table-bordered')
 
-  sortedTable.classList.add('table')
-  sortedTable.appendChild(rows[0])
+  var headerOfSortedTable = sortedTable.createTHead()
+  headerOfSortedTable.appendChild(rows[0])
+
+  var bodyOfSortedTable = sortedTable.createTBody()
 
   for (var i = 0; i < sortedRows.length; i++) {
-    sortedTable.appendChild(sortedRows[i])
+    bodyOfSortedTable.appendChild(sortedRows[i])
   }
 
   table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRows(row1, row2) {
+function compareRowsAsc(row1, row2) {
   var testTitle1 = row1.querySelector('td').textContent
   var testTitle2 = row2.querySelector('td').textContent
 
   if (testTitle1 < testTitle2) { return -1 }
   if (testTitle1 > testTitle2) { return 1 }
+  return 0
+}
+
+function compareRowsDesc(row1, row2) {
+  var testTitle1 = row1.querySelector('td').textContent
+  var testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return 1 }
+  if (testTitle1 > testTitle2) { return -1 }
   return 0
 }
