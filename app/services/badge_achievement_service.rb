1dbd8cf4724ea72bee6edcb87c1e16c8873c5ac8
Vicxwild <vicxwild@gmail.com>
2023-03-30 12:06:03 +0300

    refactor: refine badge service

--- app/services/badge_achievement_service.rb
+++ app/services/badge_achievement_service.rb
@@ -14,23 +14,13 @@ class BadgeAchievementService
   end
 
   def call
-    new_achievements = find_new_achievements
-
-    save_badges(new_achievements) if new_achievements.any?
+    badges.map do |badge|
+      rule = RULES[badge.rule_type.to_sym]
+      @user.badges << badge if rule.suitable?(test_passage, badge)
+    end.compact
   end
 
   private
 
   attr_reader :test_passage, :user, :badges
-
-  def find_new_achievements
-    badges.map do |badge|
-      rule = RULES[badge.rule_type.to_sym]
-      badge if rule.suitable?(test_passage, badge)
-    end.compact
-  end
-
-  def save_badges(new_achievements)
-    @user.badges << new_achievements
-  end
 end
